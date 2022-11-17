cfcae0736d652694affb6c34e0c4be7d32673d06
MichaelShawUK <MichaelShawUK@gmail.com>
2022-11-17 19:30:11 +0000

    Set background image according to weather

---
+++ dom.js
@@ -0,0 +1,2 @@
+export const location = document.getElementById('location');
+export const getLocationBtn = document.getElementById('get-location-btn');
--- main.js
+++ main.js
@@ -1,20 +1,51 @@
-async function getWeatherData() {
-  const location = 'Hartlepool';
+import * as dom from './dom.js';
+
+async function getWeatherData(location) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=6f2b9a56099670677b7914027874d332`;
 
   try {
     const response = await fetch(url, {mode: 'cors'});
     if (!response.ok) throw new Error("Unable to fetch data");
     const data = await response.json();
-    console.log(data);
-    console.log(data.name, data.sys.country);
-    console.log(data.weather[0].main);
-    console.log(`${Math.round(data.main.temp - 273.15)}℃`);
-    console.log(data.weather[0].description);
-    console.log(`${Math.round(data.wind.speed * (3600/1609))} mph`);
+    return {
+      location: data.name,
+      weather: data.weather[0].main,
+      desc: data.weather[0].description,
+      temp: Math.round(data.main.temp - 273.15),
+      wind: Math.round(data.wind.speed * (3600/1609)),
+    }
   } catch(error) {
     console.error(error);
   }
 }
 
-getWeatherData();
+async function displayData(location) {
+  const data = await getWeatherData(location);
+  console.table(data);
+  return data;
+}
+
+dom.getLocationBtn.addEventListener('click', () => {
+  displayData(dom.location.value)
+    .then(res => setBackground(res));
+})
+
+function setBackground(data) {
+  console.log(data.weather);
+  switch (data.weather) {
+    case 'Clear':
+      document.body.style['background-image'] = "url('/img/clear.png')";
+      break;
+    case 'Clouds':
+      document.body.style['background-image'] = "url('/img/cloud.jpg')";
+      break;
+    case 'Rain':
+      document.body.style['background-image'] = "url('/img/rain.jpg')";
+      break;
+    case 'Snow':
+      document.body.style['background-image'] = "url('/img/snow.jpg')";
+      break;
+
+      
+  }
+}
